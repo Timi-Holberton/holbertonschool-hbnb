068f1ba2456d5a8de4c18c03389870398d2220a9
Robin-1998 <10616@holbertonstudents.com>
2025-07-03 15:04:19 +0200

    Cartographier la classe BaseModel

--- part3/app/models/BaseModel.py
+++ part3/app/models/BaseModel.py
@@ -18,16 +18,21 @@ Main features:
 Exceptions are raised to ensure data integrity.
 """
 
-import uuid
-from datetime import datetime
 from app import db
+import uuid
+from datetime import datetime, UTC # Pour les fuseaux horaires
 
 
-class BaseModel():
+
+class BaseModel(db.Model):
+    __abstract__ = True  # This ensures SQLAlchemy does not create a table for BaseModel
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.now(UTC))
+    updated_at = db.Column(db.DateTime, default=datetime.now(UTC), onupdate=datetime.now(UTC)) #)
+    
     def __init__(self):
         self.id = str(uuid.uuid4())
-# nous stockons le UUIDgénéré sous forme de fichier a String pour éviter les
-# problèmes lors de la récupération à partir du référentiel de mémoire.
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
