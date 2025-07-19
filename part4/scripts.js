7c40d38b5cf105ad80c5c088f660e7841572c824
Timi-Holberton <timi.holberton@gmail.com>
2025-07-19 22:27:30 +0200

    Ajout de la gestion des avis pour les lieux, création dynamique de sections d'avis si elles n'existent pas.

--- part4/scripts.js
+++ part4/scripts.js
@@ -1,4 +1,4 @@
-/* 
+/*
   This is a SAMPLE FILE to get you started.
   Please, follow the project instructions to complete the tasks.
 */
@@ -6,3 +6,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     /* DO SOMETHING */
   });
+
+
+document.querySelectorAll('.review-card').forEach(review => {
+    const placeName = review.dataset.place;
+    const place = document.querySelector(`.place-info[data-place-name="${placeName}"]`);
+
+    if (place) {
+        // Crée une div pour les reviews si elle n’existe pas
+        let reviewSection = place.querySelector('.place-reviews');
+        if (!reviewSection) {
+            reviewSection = document.createElement('div');
+            reviewSection.classList.add('place-reviews');
+            place.appendChild(reviewSection);
+        }
+
+        reviewSection.appendChild(review);
+    }
+});
