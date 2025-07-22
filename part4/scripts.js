2104f1aecd5eea92b60f5b5dcc87fc9db858f94e
Timi-Holberton <timi.holberton@gmail.com>
2025-07-22 16:30:22 +0200

    Correction erreur dans le chemin pour auth

--- part4/scripts.js
+++ part4/scripts.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 async function loginUser(email, password) {
-    const response = await fetch('http://localhost:5000/api/v1/login', {
+    const response = await fetch('http://localhost:5000/api/v1/auth/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
